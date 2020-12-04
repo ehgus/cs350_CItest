d3e0c7662bd31f8540ea9253bc4b15871292daeb
changgeon <rhckdrjs@gmail.com>
2020-12-04 13:18:28 +0900

    Update CalculatorTest.java
    make button-click test as comment, rollback to previous testcases

--- src/test/java/simplejavacalculator/CalculatorTest.java
+++ src/test/java/simplejavacalculator/CalculatorTest.java
@@ -49,10 +49,10 @@ public class CalculatorTest
 
     @Test
     public void add() {
-//        3+2 = 5
-//        calc.calculateBi(Calculator.BiOperatorModes.add, 3.0);
-//        assertEquals(calc.calculateEqual(2.0), 5, 0);
-
+        // 3+2 = 5
+        calc.calculateBi(Calculator.BiOperatorModes.add, 3.0);
+        assertEquals(calc.calculateEqual(2.0), 5, 0);
+        /*
         try {
             click(1065, 500);
             click(950, 400);
@@ -63,13 +63,13 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 5, 0);
+        */
 
-//        10+0 = 10 --> may have to modify Calculator.java code later to improve quality
-//        calc.calculateBi(Calculator.BiOperatorModes.add, 10.0);
-//        assertEquals(calc.calculateEqual(0.0), 10, 0);
-
+        // 10+0 = 10 --> may have to modify Calculator.java code later to improve quality
+        calc.calculateBi(Calculator.BiOperatorModes.add, 10.0);
+        assertEquals(calc.calculateEqual(0.0), 10, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 400);
@@ -81,16 +81,16 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 10, 0);
+        */
     }
 
     @Test
     public void minus() {
-//        3-2 = 1
-//        calc.calculateBi(Calculator.BiOperatorModes.minus, 3.0);
-//        assertEquals(calc.calculateEqual(2.0), 1, 0);
-
+        // 3-2 = 1
+        calc.calculateBi(Calculator.BiOperatorModes.minus, 3.0);
+        assertEquals(calc.calculateEqual(2.0), 1, 0);
+        /*
         try {
             click(1065, 500);
             click(950, 400);
@@ -101,16 +101,16 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 1, 0);
+        */
     }
 
     @Test
     public void multiply() {
-//        15*7 = 105
-//        calc.calculateBi(Calculator.BiOperatorModes.multiply, 15.0);
-//        assertEquals(calc.calculateEqual(7.0), 105, 0);
-
+        // 15*7 = 105
+        calc.calculateBi(Calculator.BiOperatorModes.multiply, 15.0);
+        assertEquals(calc.calculateEqual(7.0), 105, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 400);
@@ -122,16 +122,16 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 105, 0);
+        */
     }
 
     @Test
     public void divide() {
-//        18/6 = 3
-//        calc.calculateBi(Calculator.BiOperatorModes.divide, 18.0);
-//        assertEquals(calc.calculateEqual(6.0), 3, 0);
-
+        // 18/6 = 3
+        calc.calculateBi(Calculator.BiOperatorModes.divide, 18.0);
+        assertEquals(calc.calculateEqual(6.0), 3, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 400);
@@ -143,13 +143,13 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 3, 0);
+        */
 
-//        4/0 = Infinity
-//        calc.calculateBi(Calculator.BiOperatorModes.divide, 4.0);
-//        assertEquals(calc.calculateEqual(0.0), Double.POSITIVE_INFINITY, 0);
-
+        // 4/0 = Infinity
+        calc.calculateBi(Calculator.BiOperatorModes.divide, 4.0);
+        assertEquals(calc.calculateEqual(0.0), Double.POSITIVE_INFINITY, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 450);
@@ -160,16 +160,16 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(String.valueOf(ui.reader()), "Infinity");
+        */
     }
 
     @Test
     public void xpowerofy() {
-//        3^3 = 27
-//        calc.calculateBi(Calculator.BiOperatorModes.xpowerofy, 3.0);
-//        assertEquals(calc.calculateEqual(3.0), 27, 0);
-
+        // 3^3 = 27
+        calc.calculateBi(Calculator.BiOperatorModes.xpowerofy, 3.0);
+        assertEquals(calc.calculateEqual(3.0), 27, 0);
+        /*
         try {
             click(1065, 500);
             click(950, 400);
@@ -180,15 +180,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 27, 0);
+        */
     }
 
     @Test
     public void square() {
-//        15^2 = 225
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.square, 15.0), 225, 0);
-
+        // 15^2 = 225
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.square, 15.0), 225, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 400);
@@ -198,15 +198,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 225, 0);
+        */
     }
 
     @Test
     public void squareRoot() {
-//        sqrt(25) = 5
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.squareRoot, 25.0), 5, 0);
-
+        // sqrt(25) = 5
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.squareRoot, 25.0), 5, 0);
+        /*
         try {
             click(1065, 500);
             click(900, 400);
@@ -216,11 +216,12 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 5, 0);
+        */
 
-//        sqrt(-25) = NaN
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.squareRoot, -25.0), Double.NaN, 0);
+        // sqrt(-25) = NaN
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.squareRoot, -25.0), Double.NaN, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 550);
@@ -236,15 +237,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 1, 0);
+        */
     }
 
     @Test
     public void oneDivideBy() {
-//        1/5 = 0.2
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.oneDevidedBy, 5.0), 0.2, 0);
-
+        //1/5 = 0.2
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.oneDevidedBy, 5.0), 0.2, 0);
+        /*
         try {
             click(1065, 500);
             click(900, 450);
@@ -253,14 +254,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 0.2, 0);
+        */
     }
 
     @Test
     public void abs() {
-//        abs(0-1) = 1
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.abs, -1.0), 1, 0);
+        // abs(0-1) = 1
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.abs, -1.0), 1, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 550);
@@ -272,11 +274,12 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 1, 0);
+        */
 
-//        abs(1) = 1
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.abs, 1.0), 1, 0);
+        // abs(1) = 1
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.abs, 1.0), 1, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 400);
@@ -285,14 +288,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 1, 0);
+        */
     }
 
     @Test
     public void sin() {
-//        sin(0) = 0
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.sin, 0.0), 0, 0);
+        // sin(0) = 0
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.sin, 0.0), 0, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 550);
@@ -301,14 +305,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 0, 0);
+        */
     }
 
     @Test
     public void cos() {
-//        cos(0) = 1
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.cos, 0.0), 1, 0);
+        // cos(0) = 1
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.cos, 0.0), 1, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 550);
@@ -317,14 +322,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 1, 0);
+        */
     }
 
     @Test
     public void tan() {
-//        tan(0) = 0
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.tan, 0.0), 0, 0);
+        //tan(0) = 0
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.tan, 0.0), 0, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 550);
@@ -333,11 +339,12 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 0, 0);
+        */
 
-//        tan(90) = NaN
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.tan, 90.0), Double.NaN, 0);
+        // tan(90) = NaN
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.tan, 90.0), Double.NaN, 0);
+        /*
         try {
             click(1065, 500);
             click(950, 500);
@@ -350,11 +357,12 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 1, 0);
+        */
 
-//        tan(7)
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.tan, 7.0), Math.tan(7), 0);
+        // tan(7)
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.tan, 7.0), Math.tan(7), 0);
+        /*
         try {
             click(1065, 500);
             click(850, 500);
@@ -363,14 +371,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), Math.tan(7), 0.2);
+        */
     }
 
     @Test
     public void log() {
-//        log10(100) = 2
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.log, 100.0), 2, 0);
+        // log10(100) = 2
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.log, 100.0), 2, 0);
+        /*
         try {
             click(1065, 500);
             click(850, 400);
@@ -381,14 +390,15 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 2, 0);
+        */
     }
 
     @Test
     public void rate() {
-//        rate(50) = 0.5
-//        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.rate, 50.0), 0.5, 0);
+        // rate(50) = 0.5
+        assertEquals(calc.calculateMono(Calculator.MonoOperatorModes.rate, 50.0), 0.5, 0);
+        /*
         try {
             click(1065, 500);
             click(900, 450);
@@ -398,13 +408,14 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 0.5, 0);
+         */
     }
 
+    /*
     @Test
     public void bin() {
-//        bin(5) = 101
+        // bin(5) = 101
         try {
             click(1065, 500);
             click(900, 450);
@@ -413,11 +424,10 @@ public class CalculatorTest
         catch(Exception e) {
             System.out.println(e.getMessage());
         }
-
         assertEquals(ui.reader(), 101, 0);
     }
+     */
 
-    /*
     @Test
     public void equal() {
         assertEquals(calc.calculateEqual(30.0), Double.NaN, 0);
@@ -429,6 +439,7 @@ public class CalculatorTest
         assertEquals(calc.reset(), Double.NaN, 0);
     }
 
+    /*
     @Test
     public void ui() {
         String[] args = null;
